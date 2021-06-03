8408801613f1ecb5c24864f3e62a3f0c79ed868d
Tung <tung.phan@opes.vn>
2021-06-03 14:22:08 +0700

    env

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", env("PORT")),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '86ede22715db2b27a9eaa53503720a2b'),
+      secret: env("ADMIN_JWT_SECRET", "86ede22715db2b27a9eaa53503720a2b"),
     },
   },
 });
